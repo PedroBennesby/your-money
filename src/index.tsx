89830adb1375be89f2af650582f88211b0beb0d2
Pedro Bennesby <pedrobennesby@gmail.com>
2021-12-01 11:21:18 -0300

    feat: :sparkles: Requisições api

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,34 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: '1000',
+          createdAt: new Date('2020-01-01 00:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: '1100',
+          createdAt: new Date('2020-01-01 10:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions');
+      return this.schema.db.transactions;
     });
 
     this.post('/transactions', (schema, request) => {
